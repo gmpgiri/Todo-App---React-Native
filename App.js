92a04291d0b7290d4fbb9951cbf6e6d396a3455c
Giri Madhan <gmpgiri@gmail.com>
2020-10-07 15:40:04 +0530

    added a alert box for when no text is entered in the box

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import AddToDo from "./components/AddToDo/AddToDo";
 import Header from "./components/Header/Header";
 import TodoItem from "./components/TodoItem/TodoItem";
@@ -18,9 +18,22 @@ export default function App() {
   };
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
-    });
+    if (text.length > 0) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert(
+        "Man!",
+        "Enter something in the box before pressing the button",
+        [
+          {
+            text: "Got it",
+            onPress: () => console.log("alert closed"),
+          },
+        ]
+      );
+    }
   };
 
   return (
