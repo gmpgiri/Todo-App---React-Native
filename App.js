2d914c8b6579c735c1d404e82d8be62b513c8e8e
Giri Madhan <gmpgiri@gmail.com>
2020-10-07 15:30:59 +0530

    added a option to add todos and delete todos

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import AddToDo from "./components/AddToDo/AddToDo";
+import Header from "./components/Header/Header";
+import TodoItem from "./components/TodoItem/TodoItem";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  const [todos, setTodos] = useState([
+    { text: "add the logo to the web page", key: "1" },
+    { text: "finish the book", key: "2" },
+    { text: "play a game or two", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <AddToDo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
---
+++ components/AddToDo/AddToDo.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+
+export default function AddToDo({ submitHandler }) {
+  const [text, setText] = useState("");
+
+  const changeHandler = (val) => {
+    setText(val);
+  };
+
+  return (
+    <View>
+      {/* onChangeText = (val) => changeHandler(val) */}
+      <TextInput
+        style={styles.input}
+        placeholder='Add a todo...'
+        onChangeText={changeHandler}
+      />
+      <Button
+        onPress={() => submitHandler(text)}
+        title='Add Todo'
+        color='coral'
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+});
